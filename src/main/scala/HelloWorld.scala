4e1d7d0ba5c4b0bfd7eb8229ca86a333c53ebc54
Major <major.mei@ucloud.cn>
2018-01-11 09:55:44 +0800

    add maven

---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,14 @@
+import scala.collection.immutable.HashSet
+import scala.collection.mutable
+
+object HelloWorld {
+  def main(args: Array[String]) {
+    val hashSet = HashSet("Tomatoes", "Chilies")
+    println(hashSet + "Coriander")
+
+    val testMap = mutable.Map[String,String]()
+    testMap += ("major" -> "lonly")
+    println(testMap("major"))
+  }
+
+}
---
+++ src/main/scala/Iotest.scala
@@ -0,0 +1,12 @@
+import scala.io.Source
+
+object Iotest {
+  def main(args: Array[String]): Unit = {
+    if (args.length > 0) {
+      for (line <- Source.fromFile(args(0)).getLines())
+        println(line.length + " " + line)
+    }
+    else
+      Console.err.println("set the filename")
+  }
+}
